98c1040101c20ca0160927d3a75961edacc7a930
Andrew Abosh <andrewaboshartworks@gmail.com>
2021-04-17 20:52:18 -0400

    Customize preferences tint color

--- prefs/SIMRootListController.m
+++ prefs/SIMRootListController.m
@@ -10,4 +10,16 @@
 	return _specifiers;
 }
 
+- (instancetype)init {
+    self = [super init];
+
+    if (self) {
+        HBAppearanceSettings *appearanceSettings = [[HBAppearanceSettings alloc] init];
+        appearanceSettings.tintColor = [UIColor colorWithRed: 0.23 green: 0.70 blue: 0.72 alpha: 1.00];
+        self.hb_appearanceSettings = appearanceSettings;
+    }
+
+    return self;
+}
+
 @end
